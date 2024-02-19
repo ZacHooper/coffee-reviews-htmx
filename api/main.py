86d0ab585adb01b87a604604f9970a2be0b4f27b
Zac Hooper <zac.g.hooper@gmail.com>
2024-02-20 09:44:31 +1100

    feat: add success page

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from mangum import Mangum
 
 app = FastAPI()
 
@@ -24,4 +25,12 @@ async def review(request: Request):
     # Get the form data
     form = await request.form()
     print(form)
+    return templates.TemplateResponse(request=request, name="success.html")
+
+
+@app.get("/api/review", response_class=HTMLResponse)
+async def review(request: Request):
     return templates.TemplateResponse(request=request, name="form.html")
+
+
+handler = Mangum(app)
