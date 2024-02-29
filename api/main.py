dd5002c5cc163dc7cd2c139c80b7d7fee2265daf
Zac Hooper <zac.g.hooper@gmail.com>
2024-02-29 16:04:18 +1100

    feat: Add & Delete Coffees

--- api/main.py
+++ api/main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from mangum import Mangum
 from utils import s3
+import uuid
 
 app = FastAPI()
 
@@ -40,8 +41,53 @@ async def review(request: Request):
 
 
 @app.get("/api/pantry", response_class=HTMLResponse)
-async def new_coffee(request: Request):
-    return templates.TemplateResponse(request=request, name="pantry.html")
+async def show_pantry(request: Request):
+    coffees = s3.get_active_coffees(BUCKET)
+    return templates.TemplateResponse(
+        request=request, name="pantry.html", context={"coffees": coffees}
+    )
+
+
+# no response
+@app.post("/api/coffees", response_class=HTMLResponse)
+async def show_pantry(request: Request):
+    form = await request.form()
+    active_coffees = s3.get_all_coffees(BUCKET)
+    # TODO: handle coffee already exists. Ask if they want to refill instead
+
+    form_data = dict(form)
+    form_data["active"] = "True"
+    form_data["id"] = str(uuid.uuid4())
+
+    # add the new coffee to the list of active coffees
+    active_coffees.append(form_data)
+
+    s3.update_coffees(active_coffees, BUCKET)
+    return templates.TemplateResponse(request=request, name="new_coffee_form.html")
+
+
+@app.delete("/api/coffees/{coffee_id}", response_class=HTMLResponse)
+async def show_pantry(request: Request, coffee_id: str):
+    all_coffees = s3.get_all_coffees(BUCKET)
+    # change the coffee to inactive
+    updated_coffee = [coffee for coffee in all_coffees if coffee["uuid"] == coffee_id][
+        0
+    ]
+    updated_coffee["active"] = False
+
+    # update the coffee list
+    all_coffees = [
+        coffee if coffee["uuid"] != coffee_id else updated_coffee
+        for coffee in all_coffees
+    ]
+
+    s3.update_coffees(all_coffees, BUCKET)
+
+    # get the active coffees
+    active_coffees = s3.get_active_coffees(BUCKET)
+    return templates.TemplateResponse(
+        request=request, name="pantry.html", context={"coffees": active_coffees}
+    )
 
 
 handler = Mangum(app)
--- api/utils/s3.py
+++ api/utils/s3.py
@@ -28,3 +28,27 @@ def get_active_coffees(bucket: str) -> list[dict]:
     # Filter for active coffees
     active_coffees = [coffee for coffee in all_coffees if coffee["active"]]
     return active_coffees
+
+
+def get_all_coffees(bucket: str) -> list[dict]:
+    """Get the active coffees from S3"""
+    s3 = boto3.client("s3")
+    key = "coffees.json"
+    # Download the coffees.json file
+    all_coffees_raw = s3.get_object(Bucket=bucket, Key=key)
+
+    # Load the JSON
+    all_coffees = json.loads(all_coffees_raw["Body"].read())
+    return all_coffees
+
+
+def update_coffees(coffees: list[dict], bucket: str) -> bool:
+    """Update the active coffees in S3"""
+    s3 = boto3.client("s3")
+    key = "coffees.json"
+    try:
+        s3.put_object(Bucket=bucket, Key=key, Body=json.dumps(coffees))
+        return True
+    except Exception as e:
+        print(e)
+        return False
